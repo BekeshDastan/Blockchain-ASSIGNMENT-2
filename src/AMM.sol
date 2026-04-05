2ecbc1161ed65eb0c3b56ac00731ca0eb7f79f60
Bekesh Dastan <bekesdastan@gmail.com>
2026-04-05 16:58:54 +0500

    11

--- src/AMM.sol
+++ src/AMM.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./LPToken.sol";
 
 contract AMM {
@@ -11,6 +10,15 @@ contract AMM {
 
     uint256 public reserve0;
     uint256 public reserve1;
+
+    bool private _locked;
+
+    modifier nonReentrant() {
+        require(!_locked, "ReentrancyGuard: reentrant call");
+        _locked = true;
+        _;
+        _locked = false;
+    }
 
     event LiquidityAdded(address indexed provider, uint256 amount0, uint256 amount1, uint256 lpMinted);
     event LiquidityRemoved(address indexed provider, uint256 amount0, uint256 amount1, uint256 lpBurned);
@@ -22,11 +30,9 @@ contract AMM {
         lpToken = new LPToken();
     }
 
-    function addLiquidity(uint256 amount0Desired, uint256 amount1Desired) external returns (uint256 shares) {
-        token0.transferFrom(msg.sender, address(this), amount0Desired);
-        token1.transferFrom(msg.sender, address(this), amount1Desired);
-
+    function addLiquidity(uint256 amount0Desired, uint256 amount1Desired) external nonReentrant returns (uint256 shares) {
         uint256 _totalSupply = lpToken.totalSupply();
+        
         if (_totalSupply == 0) {
             shares = _sqrt(amount0Desired * amount1Desired);
         } else {
@@ -37,26 +43,37 @@ contract AMM {
         }
 
         require(shares > 0, "Insufficient liquidity minted");
-        lpToken.mint(msg.sender, shares);
+
+        token0.transferFrom(msg.sender, address(this), amount0Desired);
+        token1.transferFrom(msg.sender, address(this), amount1Desired);
+
         _updateReserves();
+        
+        lpToken.mint(msg.sender, shares);
         
         emit LiquidityAdded(msg.sender, amount0Desired, amount1Desired, shares);
     }
 
-    function removeLiquidity(uint256 shares) external returns (uint256 amount0, uint256 amount1) {
+    function removeLiquidity(uint256 shares) external nonReentrant returns (uint256 amount0, uint256 amount1) {
         uint256 _totalSupply = lpToken.totalSupply();
+        require(_totalSupply > 0, "No liquidity");
+
         amount0 = (shares * reserve0) / _totalSupply;
         amount1 = (shares * reserve1) / _totalSupply;
 
         lpToken.burn(msg.sender, shares);
+
+        reserve0 -= amount0;
+        reserve1 -= amount1;
+
         token0.transfer(msg.sender, amount0);
         token1.transfer(msg.sender, amount1);
-
-        _updateReserves();
+        
         emit LiquidityRemoved(msg.sender, amount0, amount1, shares);
     }
 
-    function swap(address tokenIn, uint256 amountIn, uint256 minAmountOut) external returns (uint256 amountOut) {
+    function swap(address tokenIn, uint256 amountIn, uint256 minAmountOut) external nonReentrant returns (uint256 amountOut) {
+        require(amountIn > 0, "Inadequate input amount");
         require(tokenIn == address(token0) || tokenIn == address(token1), "Invalid token");
         bool isToken0 = tokenIn == address(token0);
         
@@ -64,29 +81,28 @@ contract AMM {
             ? (token0, token1, reserve0, reserve1) 
             : (token1, token0, reserve1, reserve0);
 
-        tIn.transferFrom(msg.sender, address(this), amountIn);
-
         amountOut = getAmountOut(amountIn, resIn, resOut);
         require(amountOut >= minAmountOut, "Slippage: Output too low");
 
-        tOut.transfer(msg.sender, amountOut);
-        _updateReserves();
+        tIn.transferFrom(msg.sender, address(this), amountIn);
         
-        emit Swap(msg.sender, tokenIn, amountIn, amountOut);
-    }
+        tOut.transfer(msg.sender, amountOut);
+        
+        _updateReserves(); 
 
-    function getAmountOut(uint256 amountIn, uint256 resIn, uint256 resOut) public pure returns (uint256) {
-        require(amountIn > 0, "Inadequate input amount");
-        require(resIn > 0 && resOut > 0, "Inadequate liquidity");
-        uint256 amountInWithFee = amountIn * 997;
-        uint256 numerator = amountInWithFee * resOut;
-        uint256 denominator = (resIn * 1000) + amountInWithFee;
-        return numerator / denominator;
+        emit Swap(msg.sender, tokenIn, amountIn, amountOut);
     }
 
     function _updateReserves() internal {
         reserve0 = token0.balanceOf(address(this));
         reserve1 = token1.balanceOf(address(this));
+    }
+
+    function getAmountOut(uint256 amountIn, uint256 resIn, uint256 resOut) public pure returns (uint256) {
+        uint256 amountInWithFee = amountIn * 997;
+        uint256 numerator = amountInWithFee * resOut;
+        uint256 denominator = (resIn * 1000) + amountInWithFee;
+        return numerator / denominator;
     }
 
     function _sqrt(uint256 y) internal pure returns (uint256 z) {
@@ -97,9 +113,7 @@ contract AMM {
                 z = x;
                 x = (y / x + x) / 2;
             }
-        } else if (y != 0) {
-            z = 1;
-        }
+        } else if (y != 0) { z = 1; }
     }
 
     function _min(uint256 x, uint256 y) internal pure returns (uint256) {
